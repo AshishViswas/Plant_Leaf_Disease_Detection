5b4f61966ce0ccb23f864369d9f4274997d9956c
AshishViswas <ashishexotic123@gmail.com>
2024-06-30 12:45:42 +0530

    Final Commit

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 import streamlit as st
 
 working_dir = os.path.dirname(os.path.abspath(__file__))
-model_path = f"{working_dir}/trained_model/Plant_Leaf_Disease_Detector.h5"
+model_path = f"{working_dir}/Plant_Leaf_Disease_Detector.h5"
 
 #Loading the pre-trained model
 model = tf.keras.models.load_model(model_path)
